390a254c7302021019e396b04d49c449df23c8f4
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-01 22:37:42 +0200

    completed raccolta dati e lavorazione dati

--- script.js
+++ script.js
@@ -3,49 +3,61 @@ console.log('JS OK')
 
 // 1.preparazione dati
     //1.recupero l'elemento dal Dom
-    const resultKm = document.getElementById('km');
-    console.log('id', resultKm);
-    const resultAge = document.getElementById('age');
-    console.log('id', resultAge);
-    const resultPrice = document.getElementById('price');
+    const fullNameField = document.getElementById('fullname');
+    console.log('fullnamefueld', fullNameField);
+
+    const kmField = document.getElementById('km');
+    console.log('kmfiel', kmField);
+
+    const ageField = document.getElementById('age');
+    console.log('agefield', ageField);
+
+    const sendButton = document.getElementById('send-btn');
+
+    const cancelButton = document.getElementById('cancel-btn');
+   /* const resultPrice = document.getElementById('price');
     console.log('id', resultPrice);
     const resultDiscount = document.getElementById('discounted');
     console.log('id', resultDiscount);
     const resultPriceDiscounted = document.getElementById('price-discounted');
-    console.log('id', resultPriceDiscounted);
+    console.log('id', resultPriceDiscounted);*/
 // fine
 
-//2.raccolta dati
-    // 1.chiedo all'utente quanti chilometri deve percorrere
-    const userKm = parseInt(prompt('Quanti chilometri devi percorrere?', 150 ));
-    console.log('Km', userKm); 
+// creo evento
+sendButton.addEventListener('click', function() {
+    //2.raccolta dati
+        // 1.chiedo all'utente il nomee cognome
+        const fullname = fullNameField.value.trim();
+        console.log('Fullname', fullname)
+        // 2.chiedo all'utente quanti chilometri deve percorrere
+        const userKm = parseInt(kmField.value.trim());
+        console.log('Km', userKm)
 
-    // 2.chiedo all'utente la sua età
-    const userAge = parseInt(prompt('Quanti anni hai?', 15 ));
-    console.log('Age', userAge); 
+        // 3.chiedo la fascia della sua età
+        // recupero la value dell'option della select
+        let userAge = ageField.value;
+        // conversione value
+        userAge = parseInt(userAge);
+        console.log('age', userAge)
 
-     // ! validazione
-     if (userAge <= 0 || isNaN(userAge) || isNaN(userKm)){
-        alert('Uno dei valori che hai inserito non valido');
-        location.reload;
-    }
-// fine
-   
-// 3.lavorazione dati
-    // calcolo prezzo del biglietto  scontato       
+
+    // fine
+    
+    // 3.lavorazione dati
+    // calcolo prezzo del biglietto       
     const ticketKm = 0.21;
 
-    const ticketPrice = userKm * ticketKm;
+    const ticketPrice = (userKm * ticketKm).toFixed(2);
     console.log('Ticket price €', ticketPrice,);
   
     // calcolo dello sconto
     let discount = 0;
 
-    if (userAge > 65) {
+    if (userAge === 65) {
         discount = 40;
         let messageDiscount = `Hai diritto a uno sconto del ${discount}%`;
         console.log('Message Discount', messageDiscount);
-    } else if (userAge < 18) {
+    } else if (userAge === 18) {
         discount = 20;
         let messageDiscount = `Hai diritto a uno sconto del ${discount}%`;
         console.log('Message Discount', messageDiscount);
@@ -54,32 +66,33 @@ console.log('JS OK')
     // calcolo prezzo del biglietto scontato 
     let ticketDiscounted;
 
-    if (userAge < 18 || userAge > 65){  
+    if (userAge === 65 || userAge === 18){  
     console.log('discount', discount,'%');
 
     ticketDiscounted = (ticketPrice - ((ticketPrice * discount) / 100)).toFixed(2);
     console.log('Ticket price with discount €', ticketDiscounted,);
     }
+})
     
 // fine
 
 // 4.generazione output
     // stampo in pagina i km
-    resultKm.innerHTML += ` <strong> ${userKm} </strong>`;
+    // resultKm.innerHTML += ` <strong> ${userKm} </strong>`;
 
     // stampo in pagina l'età
-    resultAge.innerHTML += ` <strong> ${userAge} </strong>`;
+    // resultAge.innerHTML += ` <strong> ${userAge} </strong>`;
 
     // stampo in pagina il prezzo del biglietto senza sconto
-    resultPrice.innerHTML += ` <strong> ${ticketPrice}€ </strong>`;
+    // resultPrice.innerHTML += ` <strong> ${ticketPrice}€ </strong>`;
 
     // stampo in pagina la percentuale dello sconto
-    resultDiscount.innerHTML += `<strong> ${discount}% </strong>`;
+    // resultDiscount.innerHTML += `<strong> ${discount}% </strong>`;
 
     // stampo in pagina il prezzo finale del biglietto con lo sconto
-    if (userAge < 18 || userAge > 65){  
+    /*if (userAge < 18 || userAge > 65){  
     resultPriceDiscounted.innerHTML += ` <strong> ${ticketDiscounted}€ </strong>`
     } else {
     resultPriceDiscounted.innerHTML += ` <strong> ${ticketPrice}€ </strong>`
-    }
+    }*/
 // fine
